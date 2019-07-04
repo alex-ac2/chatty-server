3d6b5a69234a5461dd6fba7c86ca6c5d1e746f8c
Alex Curci <alex.curci@gmail.com.com>
2019-07-04 12:30:54 +0000

    Initial add of GraphQL server running additinal express app

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // server.js
-
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4');
+
+// GraphQL
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -12,6 +15,35 @@ const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+const app = express();
+
+// Sample schema
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+  type Person {
+    name: String!
+    age: Int!
+  }
+  type Mutation {
+    createPerson(name: String!, age: Int!): Person!
+  }  
+`);
+
+let root = { hello: () => 'Hello world!' };
+
+// GraphQL route
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(5000, () => {
+  console.log("GraphQL running on port 5000...")
+});
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
